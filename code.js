a6f9be984a9adecefde28341727919b02a43fcf5
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-01-05 00:12:46 +1300

    Solution2

--- code.js
+++ code.js
@@ -1,3 +1,20 @@
+function Solution2(){
+	
+	var total=0;
+	var previous=1;
+	var current=2;
+	var sum = 0;
+	while (current <4000000){
+		if(current%2===0){
+			total=total+current;
+		}
+		sum=previous+current;
+		previous=current;
+		current=sum;
+	}
+	document.getElementById("solution2").innerHTML="Sum of even Fibonacci = "+total;
+}
+
 function Solution1(){
 	
 	var total=0;
@@ -6,5 +23,5 @@ function Solution1(){
 			total=total+i;
 		}
 	}
-	document.getElementById("solution").innerHTML="Sum of multiples = "+total;
+	document.getElementById("solution1").innerHTML="Sum of multiples = "+total;
 }
